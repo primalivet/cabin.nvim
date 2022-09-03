8f5b7f8bf05e2438fd06074a3fc6799be6e2c3cf
Primalivet <gustaf.holn@icloud.com>
2022-09-03 19:11:27 +0200

    update tmux theme

--- lua/cabin/init.lua
+++ lua/cabin/init.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 local default_config = {
-	fat_vert_split = true,
-	colored_columns = true,
-  dimmed_statusline = false,
-  dimmed_linenr = false,
+	fat_vert_split = false,
+	colored_columns = false,
+  dimmed_statusline = true,
+  dimmed_linenr = true,
 	light = false,
 	colors = {
 		none = "NONE",
